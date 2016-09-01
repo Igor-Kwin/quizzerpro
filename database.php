765cfc9546a98e2bc70f7756bb61a1729c0e00aa
Igor-Kwin <sinevor13@gmail.com>
2016-09-01 21:01:52 +0300

    first commit

---
+++ database.php
@@ -0,0 +1,12 @@
+<?php
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = "";
+$db_name = "quizzer";
+// Create mysqli object
+$link = new mysqli($db_host, $db_user, $db_pass, $db_name);
+// Error handler
+if($link->connect_error) {
+    printf("Connection failed: %s\n", $link->connect_error);
+    exit();
+}
